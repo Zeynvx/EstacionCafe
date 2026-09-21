7440efaa060ad3b9aaff68970582354fdd39f736
Sandro Mendoza <u202413406@upc.edu.pe>
2026-09-21 10:21:40 -0500

    feat: añadir pagina de postres y footer al layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio';
 import { PedidosComponent } from './pedidos/pedidos';
 import { LoginComponent } from './Login/Login';
+import { Postres } from './postres/postres';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'inicio', component: InicioComponent },
   { path: 'pedidos', component: PedidosComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'carta-cafes', component: Postres }
 ];
---
+++ src/app/postres/postres.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  imports: [RouterLink],
+  selector: 'app-postres',
+  styleUrl: './postres.css',
+  templateUrl: './postres.html',
+})
+export class Postres {}
