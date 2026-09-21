d16991b2400da60d7ad1b582ab319b39ae090284
Harol <haroljanampa2002@gmail.com>
2026-09-21 00:02:19 -0500

    feat: agregar modulo de login para personal y actualizar documentacion del README

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { PedidosComponent } from './pedidos/pedidos';
+import { LoginComponent } from './Login/Login'; // Importa tu componente de login
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'pedidos', pathMatch: 'full' },
-  { path: 'pedidos', component: PedidosComponent }
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Opcional: puedes redirigir al login o mantener 'pedidos'
+  { path: 'pedidos', component: PedidosComponent },
+  { path: 'login', component: LoginComponent }       // Registra la ruta para el login
 ];
